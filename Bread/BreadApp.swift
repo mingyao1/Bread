13c9131818ad04666a2b68aece6d5c6471827a90
mingyao1 <106688909+mingyao1@users.noreply.github.com>
2023-09-27 15:13:19 -0500

    Initial Commit

---
+++ Bread/BreadApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreadApp.swift
+//  Bread
+//
+//  Created by Mingyao Li on 9/27/23.
+//
+
+import SwiftUI
+
+@main
+struct BreadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bread/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bread
+//
+//  Created by Mingyao Li on 9/27/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
